686e39585ce60d5b9fdc4fbb8e98b61e275216d3
Ilya <selivanov.ilya@gmail.com>
2021-05-26 17:00:28 +0300

    whitespaces

--- src/NetMeter/Domain/Entities/Plan.cs
+++ src/NetMeter/Domain/Entities/Plan.cs
@@ -6,17 +6,13 @@ namespace Domain.Entities
     public class Plan
     {
         public int Id { get; set; }
-
         [Required]
         public string Name { get; set; }
-
         [Required]
         public string EndpointUrl { get; set; }
-
         [Required]
         [Range(1, 100, ErrorMessage = "Users number invalid (1-100).")]
         public int UsersNumber { get; set; } = 1;
-
         public bool IsLooped { get; set; } = false;
         public int Duration { get; set; } = 0;
 
--- src/NetMeter/Domain/Entities/Step.cs
+++ src/NetMeter/Domain/Entities/Step.cs
@@ -1,22 +1,16 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities
 {
     public class Step
     {
         public int Id { get; set; }
-
         [Required]
         public string Resource { get; set; }
-
         [Required]
         public int Method { get; set; }
-
         public string Headers { get; set; }
-
         public string Parameters { get; set; }
-
         public string Body { get; set; }
 
         public int PlanId { get; set; }
