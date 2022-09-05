a998a9eaa20e712df44900747339d8ad86285744
Ilya <selivanov.ilya@gmail.com>
2022-09-05 13:58:45 +0300

    db context relations

--- src/NMeter.Api.Settings/Data/AppDbContext.cs
+++ src/NMeter.Api.Settings/Data/AppDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using NMeter.Api.Settings.Models;
 
-namespace NMeter.Api.Data
+namespace NMeter.Api.Settings.Data
 {
     public class AppDbContext : DbContext
     {
@@ -19,5 +19,43 @@ namespace NMeter.Api.Data
         public DbSet<Header> Headers { get; set; }
 
         public DbSet<UrlParameter> UrlParameters { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Plan o2o Profile
+            modelBuilder.Entity<Plan>()
+                .HasOne(p => p.Profile)
+                .WithOne(p => p.Plan)
+                .HasForeignKey<Profile>(p => p.PlanId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            //Plan m2o PlanVariable
+            modelBuilder.Entity<Plan>()
+                .HasMany(p => p.Variables)
+                .WithOne(v => v.Plan)
+                .HasForeignKey(v => v.PlanId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            //Plan m2o Step
+            modelBuilder.Entity<Plan>()
+                .HasMany(p => p.Steps)
+                .WithOne(s => s.Plan)
+                .HasForeignKey(s => s.PlanId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            //Step m2o Header
+            modelBuilder.Entity<Step>()
+                .HasMany(s => s.Headers)
+                .WithOne(h => h.Step)
+                .HasForeignKey(h => h.StepId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            //Step m2o UrlParameter
+            modelBuilder.Entity<Step>()
+                .HasMany(s => s.Parameters)
+                .WithOne(p => p.Step)
+                .HasForeignKey(p => p.StepId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
