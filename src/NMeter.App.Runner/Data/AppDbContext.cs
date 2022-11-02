13bb4ec3506ca40232f6db91ad3fc3698c83670e
Ilya <selivanov.ilya@gmail.com>
2022-11-02 13:59:26 +0200

    runner db context

--- src/NMeter.App.Runner/Data/AppDbContext.cs
+++ src/NMeter.App.Runner/Data/AppDbContext.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using NMeter.App.Runner.Models;
 
 namespace NMeter.App.Runner.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
         
+        public DbSet<Plan> Plans { get; set; }
     }
 }
--- src/NMeter.App.Runner/Models/Profile.cs
+++ src/NMeter.App.Runner/Models/Profile.cs
@@ -2,6 +2,8 @@ namespace NMeter.App.Runner.Models
 {
     public class Profile
     {
+        public int Id { get; set; }
+        
         public int UsersNumber { get; set; }
 
         public int Duration { get; set; }
