4cf7559d3a0917d51df61d7317e9f657428d67a6
Ilya <selivanov.ilya@gmail.com>
2022-11-09 13:49:28 +0200

    runner tests

--- tests/NMeter.App.Runner.Tests/Data/PlanExecution.cs
+++ tests/NMeter.App.Runner.Tests/Data/PlanExecution.cs
@@ -12,11 +12,39 @@ namespace NMeter.App.Runner.Tests.Data
                 {
                     Id = 1,
                     Name = "Test Plan #1",
-                    BaseUrl = @"https://www.google.com"
+                    BaseUrl = @"https://www.google.com",
+                    Profile = new Profile
+                    {
+                        Id = 1,
+                        UsersNumber = 1,
+                        Duration = 0,
+                        PlanId = 1
+                    },
+                    Steps = new List<Step>
+                    {
+                        new Step
+                        {
+                            Id = 1,
+                            Path = @"/",
+                            Order = 0,
+                            Method = Method.GET,
+                            PlanId = 1
+                        },
+                        new Step
+                        {
+                            Id = 2,
+                            Path = @"/search?q=dotnet+core",
+                            Order = 0,
+                            Method = Method.GET,
+                            PlanId = 1
+                        }
+                    }
                 },
                 Execution = new Execution
                 {
-
+                    Id = 1,
+                    Status = ExecutionStatus.New,
+                    PlanId = 1
                 }
             };
         }
--- tests/NMeter.App.Runner.Tests/ExecutionBuilderTest.cs
+++ tests/NMeter.App.Runner.Tests/ExecutionBuilderTest.cs
@@ -1,10 +1,45 @@
+using NMeter.App.Runner.Models;
+using NMeter.App.Runner.Services;
+using NMeter.App.Runner.Tests.Data;
+
 namespace NMeter.App.Runner.Tests;
 
 [TestClass]
 public class ExecutionBuilderTest
 {
+    private readonly PlanExecution _planExecution = PlanExecutionData.GetData();
+
     [TestMethod]
-    public void TestMethod1()
+    public void Build_Execution_Positive_Test()
     {
+        var planExecutionInstance = new ExecutionBuilder(_planExecution).CreateThreads().Build();
+
+        Assert.IsNotNull(planExecutionInstance);
+    }
+
+    [TestMethod]
+    public void Build_Execution_Positive_Id_Test()
+    {
+        var planExecutionInstance = new ExecutionBuilder(_planExecution).CreateThreads().Build();
+
+        Assert.AreEqual(planExecutionInstance.Id, _planExecution.Execution.Id.ToString());
+    }
+
+    [TestMethod]
+    public void Build_Execution_Positive_Threads_Amount_Test()
+    {
+        var planExecutionInstance = new ExecutionBuilder(_planExecution).CreateThreads().Build();
+
+        Assert.AreEqual(planExecutionInstance.ExecutionThreads.Count, 
+            _planExecution.Plan.Profile.UsersNumber);
+    }
+
+    [TestMethod]
+    public void Build_Execution_Positive_Steps_Amount_Test()
+    {
+        var planExecutionInstance = new ExecutionBuilder(_planExecution).CreateThreads().Build();
+
+        Assert.AreEqual(planExecutionInstance.ExecutionThreads.FirstOrDefault().Value.Steps.Count, 
+            _planExecution.Plan.Steps.Count);
     }
 }
