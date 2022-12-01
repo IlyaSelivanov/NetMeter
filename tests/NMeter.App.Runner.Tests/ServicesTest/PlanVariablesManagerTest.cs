84533b3d534f530df6fbbfcfe154d014b1343868
Ilya <selivanov.ilya@gmail.com>
2022-12-01 11:22:58 +0200

    PlanVariablesManager tests

--- tests/NMeter.App.Runner.Tests/ServicesTest/PlanVariablesManagerTest.cs
+++ tests/NMeter.App.Runner.Tests/ServicesTest/PlanVariablesManagerTest.cs
@@ -30,5 +30,102 @@ namespace NMeter.App.Runner.Tests
 
             Assert.AreEqual("str0", globalVariables?.ElementAtOrDefault(0)?.Value);
         }
+
+        [TestMethod]
+        public async Task RefreshPlanVariablesAsync_Empty_Expression_Negative_Test()
+        {
+            var logger = LoggerFactory
+                .Create(configure => configure.AddConsole())
+                .CreateLogger<PlanVariablesManager>();
+            var planVariablesManager = new PlanVariablesManager(logger);
+            var httpResponseMessage = HttpResponseMessageData.GetData();
+            var globalVariables = new List<PlanGlobalVariable>
+            {
+                new PlanGlobalVariable
+                {
+                    Name = "str",
+                    Value = string.Empty,
+                    Expression = "$[0].str"
+                },
+                new PlanGlobalVariable
+                {
+                    Name = "int",
+                    Value = string.Empty,
+                    Expression = string.Empty
+                }
+            };
+
+            await planVariablesManager.RefreshPlanVariablesAsync(httpResponseMessage, globalVariables);
+
+            Assert.AreEqual(string.Empty, globalVariables?.ElementAtOrDefault(1)?.Value);
+        }
+
+        [TestMethod]
+        public async Task RefreshPlanVariablesAsync_Response_Null_Negative_Test()
+        {
+            var logger = LoggerFactory
+                .Create(configure => configure.AddConsole())
+                .CreateLogger<PlanVariablesManager>();
+            var planVariablesManager = new PlanVariablesManager(logger);
+            var globalVariables = new List<PlanGlobalVariable>
+            {
+                new PlanGlobalVariable
+                {
+                    Name = "str",
+                    Value = string.Empty,
+                    Expression = "$[0].str"
+                }
+            };
+
+
+
+            try
+            {
+                await Assert.ThrowsExceptionAsync<Exception>(() => planVariablesManager.RefreshPlanVariablesAsync(null, globalVariables));
+            }
+            catch (AssertFailedException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        public async Task RefreshPlanVariablesAsync_Variables_Null_Negative_Test()
+        {
+            var logger = LoggerFactory
+                .Create(configure => configure.AddConsole())
+                .CreateLogger<PlanVariablesManager>();
+            var planVariablesManager = new PlanVariablesManager(logger);
+            var httpResponseMessage = HttpResponseMessageData.GetData();
+        
+            try
+            {
+                await Assert.ThrowsExceptionAsync<Exception>(() => planVariablesManager.RefreshPlanVariablesAsync(httpResponseMessage, null));
+            }
+            catch (AssertFailedException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        public async Task RefreshPlanVariablesAsync_Variables_Empty_Negative_Test()
+        {
+            var logger = LoggerFactory
+                .Create(configure => configure.AddConsole())
+                .CreateLogger<PlanVariablesManager>();
+            var planVariablesManager = new PlanVariablesManager(logger);
+            var httpResponseMessage = HttpResponseMessageData.GetData();
+            var globalVariables = new List<PlanGlobalVariable>();
+        
+            try
+            {
+                await Assert.ThrowsExceptionAsync<Exception>(() => planVariablesManager.RefreshPlanVariablesAsync(httpResponseMessage, globalVariables));
+            }
+            catch (AssertFailedException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
     }
 }
