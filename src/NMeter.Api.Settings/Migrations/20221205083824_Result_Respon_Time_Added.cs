64b2573ffec86edc94ffe6b67ed98278d26fa769
Ilya <selivanov.ilya@gmail.com>
2022-12-05 10:44:33 +0200

    db migration

---
+++ src/NMeter.Api.Settings/Migrations/20221205083824_Result_Respon_Time_Added.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NMeter.Api.Settings.Migrations
+{
+    public partial class Result_Respon_Time_Added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ResponseTime",
+                table: "Results",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResponseTime",
+                table: "Results");
+        }
+    }
+}
