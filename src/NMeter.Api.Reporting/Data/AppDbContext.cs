ecb1c526603f5d1b02fd4de0d6aed519424a12c5
Ilya <selivanov.ilya@gmail.com>
2022-12-07 18:41:46 +0200

    db context constructor added

--- src/NMeter.Api.Reporting/Data/AppDbContext.cs
+++ src/NMeter.Api.Reporting/Data/AppDbContext.cs
@@ -5,6 +5,9 @@ namespace NMeter.Api.Reporting.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
         public DbSet<Result> Results => Set<Result>();
     }
 }
