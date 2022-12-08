ffb4ebf045f11b606fe16ebea321dc4ddc478a41
Ilya <selivanov.ilya@gmail.com>
2022-12-08 14:49:43 +0200

    redis distributed cache added

--- src/NMeter.Api.Reporting/Program.cs
+++ src/NMeter.Api.Reporting/Program.cs
@@ -8,13 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("NMeterDB"))
 );
+
 builder.Services
     .AddGraphQLServer()
     .AddQueryType<Query>();
 
+builder.Services.AddStackExchangeRedisCache(options =>
+    options.Configuration = builder.Configuration.GetConnectionString("RedisUrl")
+);
+
 var app = builder.Build();
 
 app.MapGraphQL();
-app.UseGraphQLVoyager();
+app.UseGraphQLVoyager(path: "/graphql-voyager");
 
 await app.RunAsync();
