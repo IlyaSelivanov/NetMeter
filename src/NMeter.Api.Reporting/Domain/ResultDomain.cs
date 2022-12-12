4c96e1784f0021a86a8dae815fbc819f27547912
Ilya <selivanov.ilya@gmail.com>
2022-12-12 13:56:28 +0200

    reporting - result domain

--- src/NMeter.Api.Reporting/Domain/ResultDomain.cs
+++ src/NMeter.Api.Reporting/Domain/ResultDomain.cs
@@ -7,17 +7,49 @@ namespace NMeter.Api.Reporting.Domain
     public class ResultDomain : IResultDomain
     {
         private readonly IDistributedCache _cache;
-        private readonly AppDbContext _context;
+        private readonly IResultRepository _resultRepository;
+        private readonly IConfiguration _configuration;
+        private int _resultsPerPage;
 
-        public ResultDomain(IDistributedCache cache, AppDbContext context)
+        public ResultDomain(
+            IDistributedCache cache,
+            IResultRepository resultRepository,
+            IConfiguration configuration)
         {
             _cache = cache;
-            _context = context;
+            _resultRepository = resultRepository;
+            _configuration = configuration;
         }
 
         public async Task<ExecutionResult> GetExecutionResult(int executionId, RequestSettings requestSettings)
         {
-            throw new NotImplementedException();
+            if (!int.TryParse(_configuration["ResultsPerPage"], out _resultsPerPage))
+                _resultsPerPage = 100;
+
+            var results = await _resultRepository.GetExecutionResults(executionId);
+
+            var totalRequestsAmount = results.Count();
+            var successAmount = results.Where(r => r.ResponseCode.Equals(200)).Count();
+            var minResponseTime = results.MinBy(r => r.ResponseTime)?.ResponseTime ?? 0;
+            var maxResponseTime = results.MaxBy(r => r.ResponseTime)?.ResponseTime ?? 0;
+
+            return new ExecutionResult
+            {
+                TotalRequestsAmount = totalRequestsAmount,
+                SuccessAmount = successAmount,
+                SuccessPercentage = successAmount / totalRequestsAmount,
+                MinResponseTime = minResponseTime,
+                MaxResponseTime = maxResponseTime,
+
+                PagedResults = new PagedResults
+                {
+                    PageIndex = requestSettings.PageIndex,
+                    TotalPages = totalRequestsAmount / _resultsPerPage,
+                    Results = results
+                        .Skip(requestSettings.PageIndex * _resultsPerPage)
+                        .Take(_resultsPerPage)
+                }
+            };
         }
     }
 }
--- src/NMeter.Api.Reporting/Models/ExecutionResult.cs
+++ src/NMeter.Api.Reporting/Models/ExecutionResult.cs
@@ -2,6 +2,27 @@ namespace NMeter.Api.Reporting.Models
 {
     public class ExecutionResult
     {
+        public int TotalRequestsAmount { get; set; }
 
+        public int SuccessAmount { get; set; }
+
+        public int SuccessPercentage { get; set; }
+
+        public long MinResponseTime { get; set; }
+
+        public long MaxResponseTime { get; set; }
+
+        public long AvgResponseTime { get; set; }
+
+        public required PagedResults PagedResults { get; set; }
+    }
+
+    public class PagedResults
+    {
+        public int PageIndex { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public required IEnumerable<Result> Results { get; set; }
     }
 }
